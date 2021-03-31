c883a4abfd98a5bd67a7f1eb36b2c5a053a3e5ce
Abutahir12 <abutahirism@gmail.com>
2021-03-31 14:12:10 +0530

    created package setup.py and updated readme.md

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    description="It's a MLops Project using winequality dataset",
+    author="whitespidey",
+    packages=find_packages(),
+    license="MIT"
+)
